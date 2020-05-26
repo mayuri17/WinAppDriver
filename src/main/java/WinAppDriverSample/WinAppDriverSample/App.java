2c7060ac6f962cefd160ac2d4b177c7681583e66
Mayuri Soudagar <mayurisoudagar17@gmail.com>
2020-05-26 02:24:40 -0700

    project files

---
+++ src/main/java/WinAppDriverSample/WinAppDriverSample/App.java
@@ -0,0 +1,13 @@
+package WinAppDriverSample.WinAppDriverSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/WinAppDriverSample/WinAppDriverSample/AppTest.java
@@ -0,0 +1,20 @@
+package WinAppDriverSample.WinAppDriverSample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
